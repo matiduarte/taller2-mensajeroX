e540a562037d6324540305d7daf9c8e08f9c7eee
quimeyfunes <quimey.funes@gmail.com>
2015-03-23 21:00:16 -0300

    Subido ejemplo de servidor corriendo y funcionando

--- Servidor/main.cpp
+++ Servidor/main.cpp
@@ -1,12 +1,32 @@
-#include <assert.h>
-#include "rocksdb/db.h"
-#include "stdio.h"
+#include "src/WebServer/mongoose.h"
+#include <stdio.h>
+#include <string.h>
 
-int main(int argc, char** argv) {
-	rocksdb::DB *db;
-	rocksdb::Options options;
-	options.create_if_missing = true;
-	rocksdb::Status status = rocksdb::DB::Open(options, "/tmp/testdb/", &db);
-	assert(status.ok());
-	return 0;
+static int ev_handler(struct mg_connection *conn, enum mg_event ev) {
+  switch (ev) {
+    case MG_AUTH: return MG_TRUE;
+    case MG_REQUEST:
+      mg_printf_data(conn, "Hello! Requested URI is [%s]", conn->uri);
+      return MG_TRUE;
+    default: return MG_FALSE;
+  }
+}
+
+int main(void) {
+  struct mg_server *server;
+
+  // Create and configure the server
+  server = mg_create_server(NULL, ev_handler);
+  mg_set_option(server, "listening_port", "8080");
+
+  // Serve request. Hit Ctrl-C to terminate the program
+  printf("Starting on port %s\n", mg_get_option(server, "listening_port"));
+  for (;;) {
+    mg_poll_server(server, 1000);
+  }
+
+  // Cleanup, and free server instance
+  mg_destroy_server(&server);
+
+  return 0;
 }
