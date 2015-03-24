13873281692f1593267556cc0098ca95834c50eb
duarte.mati@gmail.com <duarte.mati@gmail.com>
2015-03-24 19:18:37 -0300

    Subo la clase Conversacion

---
+++ Servidor/src/Entidades/Conversacion.cpp
@@ -0,0 +1,18 @@
+/*
+ * Conversacion.cpp
+ *
+ *  Created on: 24/3/2015
+ *      Author: matias
+ */
+
+#include "Conversacion.h"
+
+Conversacion::Conversacion() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Conversacion::~Conversacion() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ Servidor/src/Entidades/Conversacion.h
@@ -0,0 +1,17 @@
+/*
+ * Conversacion.h
+ *
+ *  Created on: 24/3/2015
+ *      Author: matias
+ */
+
+#ifndef SRC_ENTIDADES_CONVERSACION_H_
+#define SRC_ENTIDADES_CONVERSACION_H_
+
+class Conversacion {
+public:
+	Conversacion();
+	virtual ~Conversacion();
+};
+
+#endif /* SRC_ENTIDADES_CONVERSACION_H_ */
