b80ff7f95b05f2859fe99c3cd8dcd0005661e8a1
Juan Manuel Baracat <juanmabaracat@gmail.com>
2015-04-29 16:39:26 -0300

    Corrige el nombre la activity

--- Cliente/Mensajero/app/src/main/java/com/dk/mensajero/InformacionActivity.java
+++ Cliente/Mensajero/app/src/main/java/com/dk/mensajero/InformacionActivity.java
@@ -11,13 +11,13 @@ public class InformacionActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_imformacion);
+        setContentView(R.layout.activity_informacion);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_imformacion, menu);
+        getMenuInflater().inflate(R.menu.menu_informacion, menu);
         return true;
     }
 
