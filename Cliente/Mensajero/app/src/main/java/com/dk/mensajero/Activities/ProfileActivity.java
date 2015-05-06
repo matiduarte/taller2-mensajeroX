9079a7fdea9a7db58376c365f674cbb787182034
Juan Manuel Baracat <juanmabaracat@gmail.com>
2015-05-06 12:20:15 -0300

    Se agregan metodos en Profile para seleccionar la foto de perfil.

--- Cliente/Mensajero/app/src/main/java/com/dk/mensajero/Activities/ProfileActivity.java
+++ Cliente/Mensajero/app/src/main/java/com/dk/mensajero/Activities/ProfileActivity.java
@@ -1,15 +1,32 @@
 package com.dk.mensajero.Activities;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.ParcelFileDescriptor;
+import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.dk.mensajero.DB.DbHelper;
 import com.dk.mensajero.DB.DbHelperContract;
+import com.dk.mensajero.Entities.User;
 import com.dk.mensajero.R;
 
+import java.io.FileDescriptor;
+import java.io.IOException;
+
 public class ProfileActivity extends ActionBarActivity {
+    private static final int SELECT_PICTURE = 1;
+    private String selectedImagePath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +41,8 @@ public class ProfileActivity extends ActionBarActivity {
         return true;
     }
 
+
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
@@ -35,7 +54,67 @@ public class ProfileActivity extends ActionBarActivity {
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
+
+
+    public void selectPicture(View view) {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent,
+                "Seleccione una foto de perfil: "), SELECT_PICTURE);
+    }
+
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            if (requestCode == SELECT_PICTURE) {
+                Uri selectedImageUri = data.getData();
+                if (selectedImageUri != null) {
+                    selectedImagePath = getPath(selectedImageUri);
+                    Log.i("el profile path: ", selectedImagePath);
+                    Log.i("URI GETPATH: ", selectedImageUri.getPath());
+                    // Setearle el path al usuario
+//                    ImageView picture = (ImageView)findViewById(R.id.imageButtonProfile);
+//
+//                    try{
+//                        picture.setImageBitmap(getBitmapFromUri(selectedImageUri));
+//                    }catch (IOException e) {
+//                        Log.i("WARNING: ",e.getMessage());
+//                    }
+
+                }
+            }
+        }
+    }
+
+
+    public String getPath(Uri uri) {
+
+        // try to retrieve the image from the media store first
+        // this will only work for images selected from gallery
+        String[] projection = { MediaStore.Images.Media.DATA };
+        Cursor cursor = managedQuery(uri, projection, null, null, null);
+        if( cursor != null ){
+            int column_index = cursor
+                    .getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            cursor.moveToFirst();
+            return cursor.getString(column_index);
+        }
+        // this is our fallback here
+        return uri.getPath();
+    }
+
+
+    private Bitmap getBitmapFromUri(Uri uri) throws IOException {
+        ParcelFileDescriptor parcelFileDescriptor =
+                getContentResolver().openFileDescriptor(uri, "r");
+        FileDescriptor fileDescriptor = parcelFileDescriptor.getFileDescriptor();
+        Bitmap image = BitmapFactory.decodeFileDescriptor(fileDescriptor);
+        parcelFileDescriptor.close();
+        return image;
+    }
+
+
 }
