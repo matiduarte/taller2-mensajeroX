a9fa394912e9bcef0b23b604c14d5d44b496ed0e
quimeyfunes <quimey.funes@gmail.com>
2015-05-27 23:58:22 -0300

    Archivo faltante en el anterior commit

--- Cliente/Mensajero/app/src/main/java/com/dk/mensajero/Activities/ConversationsListActivity.java
+++ Cliente/Mensajero/app/src/main/java/com/dk/mensajero/Activities/ConversationsListActivity.java
@@ -40,7 +40,7 @@ public class ConversationsListActivity extends ActionBarActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+        // as you specify a parent activity in AndroigdManifest.xml.
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
