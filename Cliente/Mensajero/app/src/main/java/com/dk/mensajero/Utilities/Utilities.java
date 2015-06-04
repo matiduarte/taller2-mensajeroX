67cf4e33bdc771b41f95c3bb55cbbce2612a2a4e
Juan Manuel Baracat <juanmabaracat@gmail.com>
2015-06-04 03:33:07 -0300

    Agrego un nuevo Style.

--- Cliente/Mensajero/app/src/main/java/com/dk/mensajero/Utilities/Utilities.java
+++ Cliente/Mensajero/app/src/main/java/com/dk/mensajero/Utilities/Utilities.java
@@ -5,10 +5,8 @@ import android.graphics.BitmapFactory;
 import android.graphics.drawable.Drawable;
 import android.util.Base64;
 import android.util.Log;
-
-import com.dk.mensajero.RoundImage;
-
 import java.io.ByteArrayOutputStream;
+
 
 /**
  * Created by juanma on 01/06/15.
@@ -17,6 +15,7 @@ public class Utilities {
 
     /**
      * Convierte un bitmap en string.
+     *
      * @param bitmap la imagen a ser convertida.
      * @return String la imagen codificada en base64.
      */
@@ -30,8 +29,9 @@ public class Utilities {
 
     /**
      * Convierte un string en bitmap.
+     *
      * @param encodedString el string a transformar en bitmap.
-     * (precondition: encodedString debería estar en base64)
+     *                      (precondition: encodedString debería estar en base64)
      * @return bitmap (convertido desde el string recibido.)
      */
     public static Bitmap stringToBitmap(String encodedString) {
@@ -40,25 +40,25 @@ public class Utilities {
             Bitmap bitmap = BitmapFactory.decodeByteArray(encodeByte, 0, encodeByte.length);
             return bitmap;
         } catch (Exception e) {
-            Log.i("WARNING: ",e.getMessage());
+            Log.i("WARNING: ", e.getMessage());
             return null;
         }
     }
 
-    public static Bitmap cutCentredSquare(Bitmap bitmap){
-        if (bitmap.getWidth() >= bitmap.getHeight()){
+    public static Bitmap cutCentredSquare(Bitmap bitmap) {
+        if (bitmap.getWidth() >= bitmap.getHeight()) {
             bitmap = Bitmap.createBitmap(
                     bitmap,
-                    bitmap.getWidth()/2 - bitmap.getHeight()/2,
+                    bitmap.getWidth() / 2 - bitmap.getHeight() / 2,
                     0,
                     bitmap.getHeight(),
                     bitmap.getHeight()
             );
-        }else{
+        } else {
             bitmap = Bitmap.createBitmap(
                     bitmap,
                     0,
-                    bitmap.getHeight()/2 - bitmap.getWidth()/2,
+                    bitmap.getHeight() / 2 - bitmap.getWidth() / 2,
                     bitmap.getWidth(),
                     bitmap.getWidth()
             );
@@ -67,7 +67,7 @@ public class Utilities {
     }
 
 
-    public static Drawable createRoundImage(Bitmap bitmap){
+    public static Drawable createRoundImage(Bitmap bitmap) {
         return new RoundImage(bitmap);
     }
 
