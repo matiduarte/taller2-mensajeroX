ee97281ab4810c6344f6196eddf356b93f6658b3
quimeyfunes <quimey.funes@gmail.com>
2015-06-15 00:32:03 -0300

    Corrigo error en el sh

--- Servidor/convertura.sh
+++ Servidor/convertura.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 cd build/
-make tests
+make test
 lcov --capture --directory src/ --output-file coverage.info
 genhtml coverage.info --output-directory out
 xdg-open out/index.html
