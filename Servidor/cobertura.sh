cf910af38193ee82b0a37f133a974a196c325952
duarte.mati@gmail.com <duarte.mati@gmail.com>
2015-06-18 14:49:58 -0300

    Agrego la libreria de Floating Actions Buttons

--- Servidor/cobertura.sh
+++ Servidor/cobertura.sh
@@ -2,6 +2,11 @@
 
 cd build/
 make test
+
 lcov --capture --directory src/ --output-file coverage.info
+lcov -r coverage.info /usr/include/\* -o coverage.info
+lcov -r coverage.info /usr/local/include/\* -o coverage.info
+lcov -r coverage.info src/json/\* -o coverage.info
 genhtml coverage.info --output-directory out
+
 xdg-open out/index.html
