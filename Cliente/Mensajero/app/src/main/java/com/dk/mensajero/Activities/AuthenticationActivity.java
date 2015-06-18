f92f4fa55cd657cc35dae61e233f7fb7fc5aceac
duarte.mati@gmail.com <duarte.mati@gmail.com>
2015-06-18 18:00:15 -0300

    Agrego pop up para ingresar ip del servidor

--- Cliente/Mensajero/app/src/main/java/com/dk/mensajero/Activities/AuthenticationActivity.java
+++ Cliente/Mensajero/app/src/main/java/com/dk/mensajero/Activities/AuthenticationActivity.java
@@ -1,18 +1,22 @@
 package com.dk.mensajero.Activities;
 
 import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.dk.mensajero.Entities.User;
 import com.dk.mensajero.Interfaces.GetUserCallback;
 import com.dk.mensajero.R;
 import com.dk.mensajero.Service.Service;
+import com.oguzdev.circularfloatingactionmenu.library.FloatingActionButton;
 
 public class AuthenticationActivity extends ActionBarActivity implements View.OnClickListener {
 
@@ -37,13 +41,26 @@ public class AuthenticationActivity extends ActionBarActivity implements View.On
         registerLink = (TextView) findViewById(R.id.tvRegisterLink);
         bLogin.setOnClickListener(this);
         registerLink.setOnClickListener(this);
-/*
-        ImageView icon = new ImageView(this); // Create an icon
-        icon.setImageResource(R.drawable.ic_launcher);
+
+
+        this.ipPortFloatingButton();
+
+    }
+
+    public void ipPortFloatingButton(){
+        ImageView icon = new ImageView(this);
+        icon.setImageResource(R.drawable.ic_ip);
 
         FloatingActionButton actionButton = new FloatingActionButton.Builder(this)
                 .setContentView(icon)
-                .build();*/
+                .build();
+        actionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showIpPopUp();
+            }
+        });
+
 
     }
 
@@ -123,6 +140,31 @@ public class AuthenticationActivity extends ActionBarActivity implements View.On
         dialogBuilder.setMessage("Contraseña incorrecta");
         dialogBuilder.setPositiveButton("Reintentar",null);
         dialogBuilder.show();
+    }
+
+    private void showIpPopUp() {
+        LayoutInflater layoutInflater = LayoutInflater.from(AuthenticationActivity.this);
+        View promptView = layoutInflater.inflate(R.layout.ip_address_pop_up, null);
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(AuthenticationActivity.this);
+        alertDialogBuilder.setView(promptView);
+
+        final EditText editText = (EditText) promptView.findViewById(R.id.etIp);
+        // setup a dialog window
+        alertDialogBuilder.setCancelable(false)
+                .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                    }
+                })
+                .setNegativeButton("Cancelar",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                            }
+                        });
+
+        // create an alert dialog
+        AlertDialog alert = alertDialogBuilder.create();
+        alert.show();
     }
 
 }
